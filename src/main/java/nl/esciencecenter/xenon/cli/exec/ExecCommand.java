39299edd8b0ea59cda95d38c8300fe1d469735df
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-07-26 17:24:52 +0200

    Made all streams a-sync

--- src/main/java/nl/esciencecenter/xenon/cli/exec/ExecCommand.java
+++ src/main/java/nl/esciencecenter/xenon/cli/exec/ExecCommand.java
@@ -3,19 +3,17 @@ package nl.esciencecenter.xenon.cli.exec;
 import static nl.esciencecenter.xenon.cli.Main.buildXenonProperties;
 import static nl.esciencecenter.xenon.cli.ParserHelpers.getAllowedSchedulerPropertyKeys;
 import static nl.esciencecenter.xenon.cli.Utils.getJobDescription;
-import static nl.esciencecenter.xenon.cli.Utils.pipe;
 
-import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
 import nl.esciencecenter.xenon.XenonException;
-import nl.esciencecenter.xenon.utils.StreamForwarder;
 import nl.esciencecenter.xenon.cli.XenonCommand;
 import nl.esciencecenter.xenon.credentials.Credential;
 import nl.esciencecenter.xenon.schedulers.JobDescription;
 import nl.esciencecenter.xenon.schedulers.Scheduler;
 import nl.esciencecenter.xenon.schedulers.Streams;
+import nl.esciencecenter.xenon.utils.StreamForwarder;
 
 import net.sourceforge.argparse4j.inf.Namespace;
 import org.slf4j.Logger;
@@ -42,21 +40,17 @@ public class ExecCommand extends XenonCommand {
         Streams streams = scheduler.submitInteractiveJob(description);
         StreamForwarder stdinForwarder = new StreamForwarder(System.in, streams.getStdin());
         StreamForwarder stderrForwarder = new StreamForwarder(streams.getStderr(), System.err);
+        StreamForwarder stdoutForwarder = new StreamForwarder(streams.getStdout(), System.out);
+
         scheduler.waitUntilDone(streams.getJobIdentifier(), waitTimeout);
-        try {
-            // Using copy instead of StreamForwarder to pipe stdout in main thread,
-            // so close is called after all stdout has been produced
-            pipe(streams.getStdout(), System.out);
-            streams.getStdout().close();
-        } catch (IOException e) {
-            logger.info("Copy stdout failed", e);
-        }
-        stdinForwarder.terminate(1000);
-        stderrForwarder.terminate(1000);
+
+        stdinForwarder.terminate(waitTimeout);
+        stderrForwarder.terminate(waitTimeout);
+        stdoutForwarder.terminate(waitTimeout);
+
         scheduler.close();
+
         // run has no output, because all output has already been sent to stdout and stderr.
         return null;
     }
-
-
 }
