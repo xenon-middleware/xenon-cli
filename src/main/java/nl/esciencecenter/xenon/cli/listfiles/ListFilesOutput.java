46b9780f0616f0c183c1196f848a96e43f08a41f
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-07-26 14:59:03 +0200

    Handle listing of hidden files/dirs correctly.

--- src/main/java/nl/esciencecenter/xenon/cli/listfiles/ListFilesOutput.java
+++ src/main/java/nl/esciencecenter/xenon/cli/listfiles/ListFilesOutput.java
@@ -1,6 +1,8 @@
 package nl.esciencecenter.xenon.cli.listfiles;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -11,14 +13,24 @@ import nl.esciencecenter.xenon.filesystems.PathAttributes;
  * Listing of files and directories of a location
  */
 public class ListFilesOutput {
-    private Set<String> objects = new HashSet<>();
-    private Set<String> files = new HashSet<>();
-    private Set<String> directories = new HashSet<>();
+    private List<String> objects = new ArrayList<>();
+    private List<String> files = new ArrayList<>();
+    private List<String> directories = new ArrayList<>();
 
     public ListFilesOutput(Path start, Iterable<PathAttributes> items, Boolean hidden) {
+        // keep track of all hidden dirs,
+        // if path starts with one of the hidden dirs than path is also hidden
+        // only works when parent dir is given before child
+        Set<Path> hiddenDirs = new HashSet<>();
         for (PathAttributes item: items) {
-            if (!item.isHidden() || hidden) {
-                String path = start.relativize(item.getPath()).getRelativePath();
+            String path = start.relativize(item.getPath()).getRelativePath();
+            if (!hidden && item.isDirectory() && item.isHidden()) {
+                hiddenDirs.add(item.getPath());
+            }
+            // the current item is hidden or one of its parents is hidden
+            boolean parentIsHidden = hiddenDirs.stream().anyMatch(d -> item.getPath().startsWith(d));
+            boolean itemIsHidden = item.isHidden() || parentIsHidden;
+            if (!itemIsHidden || hidden) {
                 objects.add(path);
                 if (item.isDirectory()) {
                     directories.add(path);
@@ -52,5 +64,9 @@ public class ListFilesOutput {
     @Override
     public int hashCode() {
         return Objects.hash(objects, files, directories);
+    }
+
+    public List<String> getObjects() {
+        return objects;
     }
 }
--- src/test/java/nl/esciencecenter/xenon/cli/listfiles/ListFilesOutputTest.java
+++ src/test/java/nl/esciencecenter/xenon/cli/listfiles/ListFilesOutputTest.java
@@ -1,10 +1,13 @@
 package nl.esciencecenter.xenon.cli.listfiles;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import nl.esciencecenter.xenon.filesystems.Path;
 import nl.esciencecenter.xenon.filesystems.PathAttributes;
@@ -43,4 +46,103 @@ public class ListFilesOutputTest {
         String expected = "{\"objects\":[\"dir1\",\"file1\"],\"files\":[\"file1\"],\"directories\":[\"dir1\"]}";
         assertEquals(expected, result);
     }
+
+    @Test
+    public void getObjects_hiddenFile_invisible() {
+        PathAttributes file1 = mock(PathAttributes.class);
+        when(file1.isRegular()).thenReturn(true);
+        when(file1.isHidden()).thenReturn(true);
+        when(file1.getPath()).thenReturn(new Path("/start/.file1"));
+        listFilesOutput = new ListFilesOutput(new Path("/start"), Arrays.asList(file1), false);
+
+        List<String> objects = listFilesOutput.getObjects();
+
+        assertTrue("Hidden files not shown", objects.isEmpty());
+    }
+
+    @Test
+    public void getObjects_hiddenFile_visible() {
+        PathAttributes file1 = mock(PathAttributes.class);
+        when(file1.isRegular()).thenReturn(true);
+        when(file1.isHidden()).thenReturn(true);
+        when(file1.getPath()).thenReturn(new Path("/start/.file1"));
+        listFilesOutput = new ListFilesOutput(new Path("/start"), Arrays.asList(file1), true);
+
+        List<String> objects = listFilesOutput.getObjects();
+
+        assertEquals("Hidden files shown", Arrays.asList(".file1"), objects);
+    }
+
+    @Test
+    public void getObjects_hiddenNestedFile_invisible() {
+        PathAttributes dir1 = mock(PathAttributes.class);
+        when(dir1.isDirectory()).thenReturn(true);
+        when(dir1.getPath()).thenReturn(new Path("/start/dir1"));
+        PathAttributes file1 = mock(PathAttributes.class);
+        when(file1.isRegular()).thenReturn(true);
+        when(file1.isHidden()).thenReturn(true);
+        when(file1.getPath()).thenReturn(new Path("/start/dir1/.file1"));
+        listFilesOutput = new ListFilesOutput(new Path("/start"), Arrays.asList(dir1, file1), false);
+
+        List<String> objects = listFilesOutput.getObjects();
+
+        List<String> expected = Arrays.asList("dir1");
+        assertEquals("Hidden files not shown", expected, objects);
+    }
+
+    @Test
+    public void getObjects_hiddenNestedFile_visible() {
+        PathAttributes dir1 = mock(PathAttributes.class);
+        when(dir1.isDirectory()).thenReturn(true);
+        when(dir1.getPath()).thenReturn(new Path("/start/dir1"));
+        PathAttributes file1 = mock(PathAttributes.class);
+        when(file1.isRegular()).thenReturn(true);
+        when(file1.isHidden()).thenReturn(true);
+        when(file1.getPath()).thenReturn(new Path("/start/dir1/.file1"));
+        listFilesOutput = new ListFilesOutput(new Path("/start"), Arrays.asList(dir1, file1), true);
+
+        List<String> objects = listFilesOutput.getObjects();
+
+        List<String> expected = Arrays.asList("dir1", "dir1/.file1");
+        assertEquals("Hidden files not shown", expected, objects);
+    }
+
+    @Test
+    public void getObjects_hiddenDirNestedFile_invisible() {
+        PathAttributes dir1 = mock(PathAttributes.class);
+        when(dir1.isDirectory()).thenReturn(true);
+        when(dir1.isHidden()).thenReturn(true);
+        when(dir1.getPath()).thenReturn(new Path("/start/.dir1"));
+        PathAttributes file1 = mock(PathAttributes.class);
+        when(file1.isRegular()).thenReturn(true);
+        when(file1.isHidden()).thenReturn(true);
+        when(file1.getPath()).thenReturn(new Path("/start/.dir1/file1"));
+        listFilesOutput = new ListFilesOutput(new Path("/start"), Arrays.asList(dir1, file1), false);
+
+        List<String> objects = listFilesOutput.getObjects();
+
+        List<String> expected = Collections.emptyList();
+        assertEquals("Hidden files not shown", expected, objects);
+    }
+
+
+    @Test
+    public void getObjects_hiddenDirNestedFile_visible() {
+        PathAttributes dir1 = mock(PathAttributes.class);
+        when(dir1.isDirectory()).thenReturn(true);
+        when(dir1.isHidden()).thenReturn(true);
+        when(dir1.getPath()).thenReturn(new Path("/start/.dir1"));
+        PathAttributes file1 = mock(PathAttributes.class);
+        when(file1.isRegular()).thenReturn(true);
+        when(file1.isHidden()).thenReturn(true);
+        when(file1.getPath()).thenReturn(new Path("/start/.dir1/file1"));
+        listFilesOutput = new ListFilesOutput(new Path("/start"), Arrays.asList(dir1, file1), true);
+
+        List<String> objects = listFilesOutput.getObjects();
+
+        List<String> expected = Arrays.asList(".dir1", ".dir1/file1");
+        assertEquals("Hidden files not shown", expected, objects);
+    }
+
+
 }
