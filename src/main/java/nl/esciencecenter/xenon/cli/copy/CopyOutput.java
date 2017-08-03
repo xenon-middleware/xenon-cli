2fa740888f5d0d2cb572ec6a4af67b567488a69f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2017-08-03 12:28:42 +0200

    updated successful copy message

--- src/main/java/nl/esciencecenter/xenon/cli/copy/CopyOutput.java
+++ src/main/java/nl/esciencecenter/xenon/cli/copy/CopyOutput.java
@@ -24,6 +24,6 @@ public class CopyOutput {
         if (target.isLocal() && targetLocation == null) {
             targetLocation = "local";
         }
-        return String.format("Copied '%s' from location '%s' to '%s' to location '%s', %d bytes copied", source.getPath(), sourceLocation, target.getPath(), targetLocation, bytesCopied);
+        return String.format("Copied '%s' from location '%s' to '%s' at location '%s', %d bytes copied", source.getPath(), sourceLocation, target.getPath(), targetLocation, bytesCopied);
     }
 }
