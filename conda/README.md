e505437a4c42da55796771e678bc5cfaa44aee61
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-01-30 12:10:23 +0100

    Add conda recipy

---
+++ conda/README.md
@@ -0,0 +1,5 @@
+Conda recipe or xenon command line interface.
+
+# Build
+
+conda-build -c conda-forge xenon-cli
---
+++ conda/xenon-cli/build.sh
@@ -0,0 +1,3 @@
+mkdir $PREFIX/lib || true
+cp bin/xenon $PREFIX/bin/xenon
+cp lib/xenon-cli-2.0.0-all.jar $PREFIX/lib/xenon-cli-2.0.0-all.jar
