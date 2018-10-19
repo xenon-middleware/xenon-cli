9c5c04294fa2ddf472289eeeef22c6b901899666
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2018-10-19 15:15:45 +0200

    Depend on xenon-all
    Requires `./gradlew publishToMavenLocal` of all xenon deps since they are not all on bintray or jitpack

--- src/main/java/nl/esciencecenter/xenon/cli/submit/SubmitParser.java
+++ src/main/java/nl/esciencecenter/xenon/cli/submit/SubmitParser.java
@@ -20,8 +20,8 @@ public class SubmitParser implements IParser {
         ParserHelpers.addRunArguments(subparser);
 
         subparser.addArgument("--stdin").help("Path to file which is used as stdin for executable");
-        subparser.addArgument("--stdout").help("Path to file which is used as stdout for executable");
-        subparser.addArgument("--stderr").help("Path to file which is used as stderr for executable");
+        subparser.addArgument("--stdout").help("Path to file which is used as stdout for executable, where the \"%j\" is replaced with the job identifier");
+        subparser.addArgument("--stderr").help("Path to file which is used as stderr for executable, where the \"%j\" is replaced with the job identifier");
 
         subparser.addArgument("--long", "-l").help("Output long format").action(Arguments.storeTrue());
         subparser.addArgument("--name").help("Name of the job");
